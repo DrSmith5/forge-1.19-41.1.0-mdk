1701177446e1b9b57a12a84c5df247d185fa6148
DrSmith5 <drsmith5@yahoo.com>
2023-09-07 11:00:17 -0700

    Added GIT message test

--- src/main/java/net/devin/tutorialmod/TutorialMod.java
+++ src/main/java/net/devin/tutorialmod/TutorialMod.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
-
+// Testing GIT push changes
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod
